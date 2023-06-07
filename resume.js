47103baa76b825a92b0533e2ec340423e4122d04
sahr <sahrmalik@live.co.uk>
2023-06-07 15:37:31 +0100

    Initial commit

---
+++ resume.js
@@ -0,0 +1,8 @@
+const counter = document.querySelector(".view-count");
+async function updateCounter() {
+    let response = await fetch("https://uwzdq3c4j5n4ole6vnoqhg647q0qdcra.lambda-url.eu-west-2.on.aws/");
+    let data = await response.json();
+    counter.innerHTML = ` Views: ${data}`;
+}
+
+updateCounter();
