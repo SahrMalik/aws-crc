e9eec8369e9d43bdcb4280d8637327ae0de17493
sahr <sahrmalik@live.co.uk>
2024-02-11 15:33:33 +0000

    updated layout

---
+++ html5up-stellar/assets/js/crcresume.js
@@ -0,0 +1,12 @@
+const counter = document.querySelector(".view-count");
+async function updateCounter() {
+  let response = await fetch(
+    "https://uwzdq3c4j5n4ole6vnoqhg647q0qdcra.lambda-url.eu-west-2.on.aws/"
+  );
+  let data = await response.json();
+  counter.innerHTML = ` Views: ${data}`;
+}
+
+updateCounter();
+
+document.body.style.zoom = "80%";
